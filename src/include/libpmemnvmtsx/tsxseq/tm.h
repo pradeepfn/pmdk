d09ce49a9c234fcebf0a41f8a334ca26ef075898
Pradeep Fernando <pradeepfn@gmail.com>
2018-12-25 19:03:33 -0500

    tx files

---
+++ src/include/libpmemnvmtsx/tsxseq/tm.h
@@ -0,0 +1,25 @@
+
+#include "tsxseq.h"
+
+
+
+#define TM_STARTUP(numThread)         tsxseqinit() 
+#define TM_SHUTDOWN()                 tsxseqfinalize()
+  
+#define TM_THREAD_ENTER()             ({ \
+			                                         long mytid = thread_getId(); \
+			                                         bind_thread(mytid); \
+			                                      })
+#define TM_THREAD_EXIT()              /* nothing */
+
+#define TX_BEGIN(pop) tsxseqbegin();
+#define TX_ONABORT
+#define TX_END        tsxseqend();
+
+#define TM_SHARED_READ(var)           (var)
+#define TM_SHARED_READ_P(var)         (var)
+#define TM_SHARED_READ_F(var)         (var)
+  
+#define TM_SHARED_WRITE(var, val)     var = val
+#define TM_SHARED_WRITE_P(var, val)   var = val
+#define TM_SHARED_WRITE_F(var, val)   var = val
