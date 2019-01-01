6941b2ad655492871163b41367d0c299a6e252ed
Waduge Pradeep Rangana Fernando <pfernando3@aries4.cc.gatech.edu>
2019-01-01 17:53:14 -0500

    numbers script

---
+++ numbers.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+declare -a arr=("ctree")
+#declare -a lt=("seq" "tsxseq")
+declare -a lt=("redo")
+
+for bm in "${arr[@]}"
+do
+	mkdir -p results/"$bm"
+	for l in "${lt[@]}"
+	do
+		./runscript.py -c -w "$bm" -lt "$l"
+		./runscript.py -b -w "$bm" -lt "$l"
+		#./runscript.py -r -w "$bm" -lt "$l" > results/"$thrt"/"$bm"/"$bm"_t1_"$l".txt
+		./runscript.py -r -w "$bm" -lt "$l"
+	done
+done
+exit
+
+
+
