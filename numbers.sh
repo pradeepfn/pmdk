548cf11f17d4c3ffa490638a1e08a261792e39c2
Waduge Pradeep Rangana Fernando <pfernando3@aries4.cc.gatech.edu>
2019-01-02 20:05:01 -0500

    got ctree numbers

--- numbers.sh
+++ numbers.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 declare -a arr=("ctree")
-#declare -a lt=("seq" "tsxseq")
-declare -a lt=("redo")
+#declare -a lt=("seq")
+declare -a lt=("seq" "tsxseq" "redofence" "redonofence" "tsxredofence" "tsxredonofence" "undofence" "undonofence" "tsxundofence" "tsxundonofence")
 
 for bm in "${arr[@]}"
 do
@@ -11,8 +11,8 @@ do
 	do
 		./runscript.py -c -w "$bm" -lt "$l"
 		./runscript.py -b -w "$bm" -lt "$l"
-		#./runscript.py -r -w "$bm" -lt "$l" > results/"$thrt"/"$bm"/"$bm"_t1_"$l".txt
-		./runscript.py -r -w "$bm" -lt "$l"
+		./runscript.py -r -w "$bm" -lt "$l" > results/"$bm"/"$bm"_t1_"$l".txt
+		#./runscript.py -r -w "$bm" -lt "$l"
 	done
 done
 exit
--- runscript.py
+++ runscript.py
@@ -9,6 +9,7 @@ DBG=1
 #config
 __home = os.getcwd()
 __tmlib_home = '/nethome/pfernando3/nvmtsx/tmlib'
+__nvmtsx_home = '/nethome/pfernando3/nvmtsx'
 
 __ctree = 'ctree'
 __empty = ''
@@ -16,20 +17,32 @@ __empty = ''
 workload_l = []
 workload_l.append(__ctree)
 
-__seq       = 'seq'
-__tsxseq    = 'tsxseq'
-__undo      = 'undo'
-__redo      = 'redo'
-__tsxundo   = 'tsxundo'
-__tsxredo   = 'tsxredo'
+__seq            = 'seq'
+__tsxseq         = 'tsxseq'
+
+__undofence      = 'undofence'
+__undonofence    = 'undonofence'
+__tsxundofence   = 'tsxundofence'
+__tsxundonofence = 'tsxundonofence'
+
+__redofence      = 'redofence'
+__redonofence      = 'redonofence'
+__tsxredofence   = 'tsxredofence'
+__tsxredonofence   = 'tsxredonofence'
 
 logtype_l = []
 logtype_l.append(__seq)
 logtype_l.append(__tsxseq)
-logtype_l.append(__undo)
-logtype_l.append(__redo)
-logtype_l.append(__tsxundo)
-logtype_l.append(__tsxredo)
+
+logtype_l.append(__undofence)
+logtype_l.append(__undonofence)
+logtype_l.append(__tsxundofence)
+logtype_l.append(__tsxundonofence)
+
+logtype_l.append(__redofence)
+logtype_l.append(__redonofence)
+logtype_l.append(__tsxredofence)
+logtype_l.append(__tsxredonofence)
 
 parser = argparse.ArgumentParser(prog="runscript", description="script to run pmdk runscript")
 parser.add_argument('-b', dest='build', action='store_true', default=False, help="build benchmark")
@@ -82,13 +95,29 @@ def sh(cmd):
 
 def libname(lt):
     if lt == __tsxseq:
-        return __tsxseq
-    elif lt == __undo or lt == __tsxundo:
-        return __tsxundo
-    elif lt == __redo or lt == __tsxredo:
-        return __tsxredo
+        return 'tsxseq'
+    elif lt == __undofence or lt == __undonofence or lt == __tsxundofence or lt == __tsxundonofence:
+        return 'tsxundo'
+    elif lt == __redofence or lt == __redonofence or lt == __tsxredofence or lt == __tsxredonofence:
+        return 'tsxredo'
     else:
         return None
+
+
+
+def tm_header(lt):
+    if lt == __undofence or lt == __undonofence:
+        return 'undo'
+    elif lt == __tsxundofence or lt == __tsxundonofence:
+        return 'tsxundo'
+    elif lt == __redofence or lt == __redonofence:
+        return 'redo'
+    elif lt == __tsxredofence or lt == __tsxredonofence:
+        return 'tsxredo'
+    else:
+        return lt
+
+
 
 
 #build nvmtsx with requested logging scheme found in
@@ -98,7 +127,7 @@ def build_libpmemnvmtsx():
 
     lib_h = 'src/libpmemnvmtsx'
     cd(lib_h)
-    cmd = 'make TMBUILD='+  ltype + ' TMLIBDIR=' + __tmlib_home
+    cmd = 'make TMBUILD='+  tm_header(ltype) + ' TMLIBDIR=' + __tmlib_home
     sh(cmd)
     cd(__home)
 
@@ -108,11 +137,11 @@ def build_ctree():
     map_h = 'src/examples/libpmemobj'
     #build c-tree first
     cd(map_h + '/tree_map')
-    cmd = 'make TMBUILD='+  ltype + ' TMLIBDIR=' + __tmlib_home
+    cmd = 'make TMBUILD='+  tm_header(ltype) + ' TMLIBDIR=' + __tmlib_home
     sh(cmd)
     cd(__home)
     cd(map_h + '/map')
-    cmd = 'make TMBUILD='+  ltype + ' TMLIBDIR=' + __tmlib_home
+    cmd = 'make TMBUILD='+  tm_header(ltype) + ' TMLIBDIR=' + __tmlib_home
     sh(cmd)
     cd(__home)
 
@@ -127,10 +156,29 @@ def build_bench():
     cd(bench_h)
     if libname(ltype) is not None:
         __tmlib_path = __tmlib_home + '/build/lib' + libname(ltype) + '.a'
-    cmd = 'make TMBUILD='+  ltype + ' TMLIBDIR=' + __tmlib_home +\
+    cmd = 'make TMBUILD='+  tm_header(ltype) + ' TMLIBDIR=' + __tmlib_home +\
           ' TMLIBPATH=' + __tmlib_path
     sh(cmd)
     cd(__home)
+
+def build_tmlib():
+    ltype = args.logtype
+    #print 'pmdk_home = ' + __home
+    cd('../nvmtsx')
+    cmd = './runscript.py -b -w tmlib -lt ' + ltype   
+    sh(cmd)
+    cd(__home)
+
+
+def clean_tmlib():
+    ltype = args.logtype
+
+    cd('../nvmtsx')
+    cmd = './runscript.py -c -w tmlib -lt ' + ltype   
+    sh(cmd)
+    cd(__home)
+
+
 
 
 def clean():
@@ -158,7 +206,8 @@ def clean():
 
 
 def run_bench():
-    cmd = "./run_ctree.sh --small"
+    #cmd = "./run_ctree.sh --small"
+    cmd = "./run_ctree.sh --large"
     sh(cmd)
 
 
@@ -172,8 +221,10 @@ if __name__ == '__main__':
     if args.build is True:
         build_libpmemnvmtsx()
         build_ctree()
+        build_tmlib()
         build_bench()
     if args.clean is True:
         clean()
+        clean_tmlib()
     if args.run is True:
         run_bench()
